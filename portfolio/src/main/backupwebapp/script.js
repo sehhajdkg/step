d6aa613a86a789d03db377f844f0b029f402833e
sehhaj <sehhajdkg@gmail.com>
2021-01-06 02:49:56 +0000

    Testing push

---
+++ portfolio/src/main/backupwebapp/script.js
@@ -0,0 +1,65 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Adds in navigation bar to each page.
+document.addEventListener('DOMContentLoaded', loadNavBar);
+function loadNavBar(){ 
+    document.getElementById("nav-bar").innerHTML='<object type="text/html" data="nav-bar.html" width=100% height="50"></object >';
+}
+
+/**
+ * Adds a random greeting to the welcome home page.
+ */
+function addRandomGreeting() {
+  const greetings =
+      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+
+  // Pick a random greeting.
+  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+
+  // Add it to the page.
+  const greetingContainer = document.getElementById('greeting-container');
+  greetingContainer.innerText = greeting;
+}
+
+/**
+ * Adds a random tv quote to the welcome home page.
+ */
+function addRandomTVQuote() {
+  const tvQuotes =
+      ['"Great, Get In There And Operate, Doctor Google" - Kendall Roy [Succession]', 
+      '“Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way." – Michael Scott [The Office]', 
+      '"If Anyone Is Feeling Anxious Or Worried Or Even If You Just Want To Chat, Please, Please, Do Not Come Crying To Me.” - Sister Michael [Derry Girls]', 
+      '“You should wash your hands, you dirty pig! - Eric Effiong [Sex Education]', 
+      '“I Get It Now, Why Men Rule The World: No High Heels.” - Midge Maisel [The Marvelous Mrs Maisel]', 
+      '"Huzzah" - Peter III of Russia [The Great]'];
+
+  // Pick a random quote.
+  const tvQuote = tvQuotes[Math.floor(Math.random() * tvQuotes.length)];
+
+  // Add it to the page.
+  const tvQuoteContainer = document.getElementById('tvQuote-container');
+  tvQuoteContainer.innerText = tvQuote;
+}
+
+/**
+ * Using async and await to fetch personal greeting data.
+ */
+async function getDataWithAsyncAwait() {
+  console.log('Fetching some data');
+  const response = await fetch('/data');
+  const quote = await response.text();
+  document.getElementById('data-container').innerHTML = quote;
+  console.log('Personal greeting successfully displayed woohoo');
+}
--- portfolio/src/main/java/com/google/sps/servlets/TranslationServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/TranslationServlet.java
@@ -22,26 +22,29 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
+import com.google.sps.data.Comment;
 
 @WebServlet("/translate")
 public class TranslationServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
     // Get the request parameters.
-    String originalText = request.getParameter("text");
+    String originalText = request.getParameter("comment");
     String languageCode = request.getParameter("languageCode");
 
     // Do the translation.
-    // Translate translate = TranslateOptions.getDefaultInstance().getService();
-    Translate translate = TranslateOptions.newBuilder().setProjectId("project-id").setQuotaProjectId("project-id").build().getService();
+    Translate translate = TranslateOptions.newBuilder().setProjectId("sehhaj-starter").setQuotaProjectId("sehhaj-starter").build().getService();
     Translation translation =
         translate.translate(originalText, Translate.TranslateOption.targetLanguage(languageCode));
     String translatedText = translation.getTranslatedText();
 
-    // Output the translation.
-    response.setContentType("text/html; charset=UTF-8");
+    // Output the translation
+    response.setContentType("text/html charset=UTF-8");
     response.setCharacterEncoding("UTF-8");
     response.getWriter().println(translatedText);
+
   }
 }
